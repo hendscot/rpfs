eff9434b3fbe78cbc20fa19ddf6fa4692d1280e1
npocodes <npocodes@gmail.com>
2017-04-24 21:50:05 -0400

    Closer! still can't return it but I've got junk...

--- np_GFS.py
+++ np_GFS.py
@@ -49,6 +49,7 @@ SUB_16 = 5 #7C2  = 21(-5) = 16
 SUB_8 = 2  #5C2  = 10(-2) = 8
 
 #A new size value for the files
+#(Max number of bytes to return!)
 FILE_SIZE = 100
 
 class MyStat(fuse.Stat):
@@ -126,6 +127,22 @@ class GFS(Fuse):
             else:
                 buf = ''
         elif path == GRAND_PATH:
+#New ALG:
+#Read bits file into 1 line per element array
+#Determine if we have enough elements for bytes requested
+  #5 elements per byte (5C2 = 10bits-2 = 8bits)
+  #If not, generate enough elements for byte request 5*size
+#For every "byte" requested:
+  #generate an 8 bit binary array ...ex:[1, 1, 0, 0, 0, 1, 1, 0]
+  #Remove used elements
+#Pack 8bit arrays into numpy.packbits
+#Each element of numpy.packbits is now 1 byte of data...
+#for each byte of data(element):
+  #convert byte to unsigned? char
+  #add char to return string
+#Run offset calculations on return string
+#EoA
+
             #offset = read offset from first byte
             #size = num of bytes/characters to read (this seems to always be 4096, idky)
             #path = the name of the file to access(gRand)
@@ -135,14 +152,14 @@ class GFS(Fuse):
               if(offset < slen):
                 if(offset + size > slen):
                   size = slen - offset
-                buf = unichr(int(self.randBytes[offset:offset+size],2))
+                buf = self.randBytes[offset:offset+size]
               else:
                 buf = ''
             else:
               #Check for randfile availability
               if(os.path.isfile(BIT_PATH) == 0):
                 #No File!
-                buf = 0
+                buf = 'Sorry, comeback later!'
               else:
                 #Open up the randFile for reading
                 fo = open(BIT_PATH, "r")
@@ -164,72 +181,104 @@ class GFS(Fuse):
                       elist.append(tmp)#add to list
                       tmp = fo.readline()   #read next line from bits file              
                     fo.close()#close the file
+                    #elist - now holds each line from the file
+                    print "randtimegeiger.txt has ", len(elist), "elements total\n"
 
                     #Determine how many elements we should use
-                    #size is in bytes...? 
-                    #1byte = 8bits
-                    #2bytes = 16bits
-                      #3bytes = 24bits
-                    #4bytes = 32bits
-                      #5bytes = 40bits
-                      #6bytes = 48bits
-                      #7bytes = 56bits
-                    #8bytes = 64bits
                     if(size < 2):
                       eNum = BIT_8
                       eSub = SUB_8
                     elif(size < 3):
                       eNum = BIT_16
                       eSub = SUB_16
-                    else:#elif(size < 5):
+                    elif(size < 5):
                       eNum = BIT_32
                       eSub = SUB_32
-                    #else:#elif(size < 9):
-                      #eNum = BIT_64
-                      #eSub = SUB_64
-                    #else:
-                      #eNum = 0 #ALL of it!
-
-                    #Determine if we should use up all rand data
-                    #Or be conservative by only using what is needed.
-                    if(eNum == 0):
-                      #USE IT ALL!
-                      wlist = [] #No remaining elements
-                      print elist, "\n"
                     else:
-                      #Make sure we have enough elements to generate
-                      #a single random number bit sequence: eNum-C-2
-                      tmpList = elist[:] #copy the list so we don't lose it
-                      while(len(elist) < eNum):
-                        random.seed(tmpList.pop(0))#remove first element, set as rand seed
-                        elist.append(str(random.random() + time.time()))#append new random number
-                      #end Generation loop
+                      eNum = BIT_64 #this is the maximum per read
+                      eSub = SUB_64
 
-                      #Save remaining elements?
-                      wlist = elist[eNum:] if (len(elist) > eNum) else []
-                      elist = elist[:eNum]#We have enough elements..take first eNum
-                      print elist, "\n"
+                    #Determine how many elements we should use
+                    #size = number of bytes requested, we need
+                    #8 bits per size or.. 8bits*size, in order
+                    #to generate 8 bits we need 5 elements so 5*size
+                    #if we don't have 5*size elements... then just
+                    #use all that we do have...? or make enough?
+                    #eNum = (5*size)#5Elements per 1byte requested
+                    #eNum neeeeds a CAP, 5x4096 = 20480
+                    #20480C2 = 209,704,960 (too many!! takes like an hour)
+                    #I believe a cap of 8bytes(64bits) is acceptable
+                    print "eNum = ", eNum, "\n"
 
-                    randNum = [] #empty list
+                    #copy the list so we don't lose it
+                    tmpList = elist[:]
+                    print "tmpList has ", len(tmpList), " elements\n"
+
+                    #Make sure we have eNum elements to use
+                    newbies = len(tmpList)#offset to new elements
+                    nRun = 1 #current run of newbies
+                    while(len(elist) < eNum):
+                      #overun check
+                      if(tmpList == []):
+                        tmpList = elist[(newbies*nRun):]#switch to new numbers generated
+                        nRun += 1
+                      #end overun check
+                      random.seed(tmpList.pop(0))#remove first element, set as rand seed
+                      tmp = str(random.random() + time.time()) #create new rand num
+                      tmp = tmp if (tmp[-3] == ".") else (tmp + "0")#pad decimal 0
+                      elist.append(tmp)#append new random number
+                    #end Generation loop
+
+                    #Save remaining elements?
+                    wlist = elist[eNum:] if (len(elist) > eNum) else []
+                    elist = elist[:eNum]#We have enough elements..take first eNum
+                    print "elist has ", len(elist), "elements\n"
+                    #elist now holds 5*size elements, no more, no less
+
+                    #Generate a 1 or 0, by comparing each 
+                    #element to each other element [(5*size)C2]
+                    randBits = [] #empty list
                     while(len(elist) > 1):#loop until only 1 element left
                       compare = elist.pop(0) #remove/set first element as compare
+                      print "Compare: ", compare[-2:], "\n"
+                      print "elist has ", len(elist), "elements\n"
+
                       for element in elist: #loop remaining elements
                         if(int(element[-2:]) > int(compare[-2:])):
-                          randNum.append('0')
+                          randBits.append(0)
                         else:
-                          randNum.append('1')
+                          randBits.append(1)
                       #list iteration finished
-                    print randNum[:-eSub], "\n"
-                    print "Count: ", len(randNum[:-eSub]), "\n"
+                    print randBits, "\n"
+                    print "randBits Count: ", len(randBits), "\n"
                     #end binary loop
+                    #randBits "list" now holds (eNumC2) 0's and 1's
                     
+                    #Seperate into 8bit(element) arrays
+                    bitLists = [] #empty list
+                    tmpList = [] #empty list
+                    for bit in randBits:
+                      tmpList.append(bit)
+                      if(len(tmpList) == 8):
+                        bitLists.append(tmpList)
+                        tmpList = []#reset tmpList
+                    #bitLists now holds multiple lists of 8 {0s or 1s}
+                    print bitLists, "\n"
+
+                    #Convert each 8bit list into string, cast to int, then character
+                    #and add it to the self.randBytes string
+                    for bitList in bitLists:
+                      self.randBytes += chr(int(''.join(str(e) for e in bitList), 2))
                     #numpy convert to array
-                    np = numpy.array(randNum[:-eSub])
-                    print np, "\n"
-                    
-                    #numpy convert to bytes
-                    self.randBytes = np.tostring()
+                    #np = numpy.array(bitLists)
+                    #print np, "\n"
                     print self.randBytes, "\n"
+
+                    #numpy convert to bytes
+                    #self.randBytes = numpy.packbits(np, 1)
+                    #print self.randBytes, "\n"
+                    #self.randBytes, should now hold multiple elements
+                    #each element being an unsigned 8bit integer
 
                     #Run offset calculations and come up
                     #With the return variable 'buf' 
@@ -237,7 +286,7 @@ class GFS(Fuse):
                     if(offset < slen):
                       if(offset + size > slen):
                        size = slen - offset
-                      buf = unichr(int(self.randBytes[offset:offset+size],2))
+                      buf = self.randBytes[offset:offset+size]
                     else:
                       buf = ''
 
